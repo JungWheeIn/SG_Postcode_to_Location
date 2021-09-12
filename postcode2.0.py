d56f0fd26cae84c19cd46ecd6bd919eb9a31470b
WheeIn <57378716+JungWheeIn@users.noreply.github.com>
2021-09-12 08:30:14 +0800

    Add files via upload

---
+++ postcode2.0.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import time
+
+file = str(input("输入txt文件位置\n"))
+
+final_f = str(input("输入输出txt位置\n"))
+
+final_file = open(final_f, 'w')
+
+result = []
+
+final = []
+
+T = int(input("请输入抓取延时\n"))
+
+with open(file, 'r') as f:
+    results = f.readlines()
+    for r in results:
+        rr = r.replace('\n', '')
+        result.append(rr)
+print(result)
+
+for code in result:
+    parameter = {
+                    'locate': code,
+                    'region': 'SG',
+                    'geoit': 'CSV',
+                }
+    '''
+    headers = {"User-Agent": "PostmanRuntime/7.28.4",
+               "Accept-Encoding": "gzip, deflate, br",
+               "Connection": "Keep-alive",
+               }
+    response = requests.get("https://geocode.xyz/?locate=location", params=parameter, headers=headers)           
+    '''
+    response = requests.get("https://geocode.xyz/?locate=location", params=parameter)
+    time.sleep(T)
+    final.append(response.text)
+    print(response.text)
+
+print(final)
+for hh in final:
+    final_file.write(hh[-17:]+' \n')
+
+f.close()
+final_file.close()
+os.system('pause')
